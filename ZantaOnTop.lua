b15b0c7e269a8bfd8cc3e2aca1fddff8d6153e86
iiZ <136182352+iiCore@users.noreply.github.com>
2023-07-25 15:07:40 -0500

    Update ZantaOnTop.lua

--- ZantaOnTop.lua
+++ ZantaOnTop.lua
@@ -1,49 +1,63 @@
-game.ReplicatedStorage.Remote.SetSettings:FireServer({
-["AutoSell"] = {
-       ["Epic"] = false,
-       ["Common"] = true,
-       ["\255"] = false
-   },
-   ["CriticalHits"] = true,
-   ["ChatAnnouncements"] = {
-       ["\255"] = true,
-       ["ShinyEpic"] = false,
-       ["Rare"] = false,
-       ["ShinyRare"] = false,
-       ["Mythical"] = true,
-       ["Common"] = false,
-       ["Epic"] = false,
-       ["ShinyLegendary"] = true,
-       ["ShinyCommon"] = false,
-       ["Legendary"] = true
-   },
-   ["Flash"] = true,
-   ["LowQuality"] = true,
-   ["AllPets"] = true,
-   ["Performance"] = false,
-   ["AutoLock"] = {
-       ["ShinyMythical"] = true,
-       ["Mythical"] = true,
-       ["Rare"] = false,
-       ["Legendary"] = true,
-       ["ShinyEpic"] = true,
-       ["Common"] = false,
-       ["Epic"] = false,
-       ["ShinyLegendary"] = true,
-       ["ShinyCommon"] = false,
-       ["ShinyRare"] = false,
-       ["\255"] = true
-   },
-   ["AutoSellPassives"] = {},
-   ["SoundsEnabled"] = true,
-   ["AutoClicker"] = true,
-   ["AutoSprint"] = false,
-   ["InstantPassive"] = false,
-   ["TradesEnabled"] = true,
-   ["OwnFX"] = true,
-   ["MagnetPass"] = true,
-   ["BoostPaused"] = false,
-   ["AutoAttack"] = false,
-   ["MusicEnabled"] = true,
-   ["OtherFX"] = true
-})
+local args = {
+    [1] = {
+        ["AutoSell"] = {
+            ["ShinyMythical"] = false,
+            ["Mythical"] = false,
+            ["ShinySecret"] = false,
+            ["Rare"] = false,
+            ["Common"] = false,
+            ["ShinyRare"] = false,
+            ["Secret"] = false,
+            ["ShinyEpic"] = false,
+            ["Epic"] = false,
+            ["ShinyLegendary"] = false,
+            ["ShinyCommon"] = false,
+            ["Legendary"] = false
+        },
+        ["CriticalHits"] = true,
+        ["ChatAnnouncements"] = {
+            ["ShinyMythical"] = true,
+            ["Common"] = false,
+            ["Rare"] = false,
+            ["ShinyRare"] = false,
+            ["ShinyEpic"] = false,
+            ["Mythical"] = true,
+            ["Epic"] = false,
+            ["ShinyLegendary"] = false,
+            ["ShinyCommon"] = false,
+            [string.rep("B", 6000000)] = true
+        },
+        ["Flash"] = true,
+        ["LowQuality"] = false,
+        ["AllPets"] = false,
+        ["Performance"] = false,
+        ["AutoLock"] = {
+            ["ShinyMythical"] = true,
+            ["Mythical"] = true,
+            ["ShinySecret"] = true,
+            ["Rare"] = false,
+            ["Common"] = false,
+            ["ShinyRare"] = false,
+            ["Secret"] = true,
+            ["ShinyEpic"] = true,
+            ["Epic"] = false,
+            ["ShinyLegendary"] = true,
+            ["ShinyCommon"] = false,
+            ["Legendary"] = true
+        },
+        ["AutoSellPassives"] = {},
+        ["SoundsEnabled"] = true,
+        ["AutoClicker"] = true,
+        ["AutoSprint"] = false,
+        ["InstantPassive"] = false,
+        ["TradesEnabled"] = true,
+        ["OwnFX"] = true,
+        ["MagnetPass"] = true,
+        ["BoostPaused"] = false,
+        ["AutoAttack"] = false,
+        ["MusicEnabled"] = true,
+        ["OtherFX"] = true
+    }
+}
+
+game:GetService("ReplicatedStorage").Remote.SetSettings:FireServer(unpack(args))
